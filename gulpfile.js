035bba7ba8070ff2e7af0b65bb1688a7d1d2da5b
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2014-02-13 00:01:04 +0000

    Use correct wildcards to watch subdirectories recursively

--- gulpfile.js
+++ gulpfile.js
@@ -13,9 +13,9 @@ var server = lr(),
     production = env === 'production';
 
 var paths = {
-    views: { watch: [ 'views/*/**.jade', 'views/*.jade', 'content/*', 'content/*/**' ], src: 'views/*/**.jade', out: 'www' },
-    browserify: { watch: [ 'src/*.coffee', 'src/*/**.coffee', 'src/*/**/***.coffee' ], src: 'src/index.coffee', out: 'www/js' },
-    styles: { watch: 'styles/*/**.styl', src: 'styles/main.styl', out: 'www/css' }
+    views: { watch: [ 'views/**/*.jade', 'content/**/*' ], src: 'views/**/*.jade', out: 'www' },
+    browserify: { watch: 'src/**/*.coffee' , src: 'src/index.coffee', out: 'www/js' },
+    styles: { watch: 'styles/**/*.styl', src: 'styles/main.styl', out: 'www/css' }
 };
 
 gulp.task('browserify', function () {
