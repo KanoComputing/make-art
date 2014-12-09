KanoComputing/make-art

commit 0570a5fa1f1e0bce91d62c622b986c0cd185c14a

Colour Ch 6's (Move that Cursor) shape yellow
The challenge introduces the idea of moving the cursor but with the
original (black) shape used in the example, the cursor was not visible.

lib/levels/06-move.js
@@ -10,7 +10,7 @@ module.exports = {
             steps[1].type === 'ellipse'
         );
     },
-    code: '###\nAdd the \'move\' spell, it will change\nthe position of the circle\n###\n\ncircle 20',
+    code: '###\nAdd the \'move\' spell, it will change\nthe position of the circle\n###\n\ncolor "yellow"\ncircle 20',
     slides: require('./06-move.md'),
     startAt: 5
 };

commit b1dadc39d6a1ea7ec3fb920b87e7551ec9c3beb8

Fix Ch 6 (Move) validation after colour change
Adding the colour change of the circle in commit
0570a5fa1f1e0bce91d62c622b986c0cd185c14a caused the number of commands
to change, which the validation relied on. Account for this change by
fixing the validate function.

lib/levels/06-move.js
@@ -3,11 +3,11 @@ module.exports = {
     description: 'Continue your work in a new place',
     validate: function (code, steps) {
         return (
-            steps.length === 2 &&
+            steps.length === 3 &&
             steps[0].type === 'moveTo' &&
             Math.abs(steps[0].options.dx) &&
             Math.abs(steps[0].options.dy) &&
-            steps[1].type === 'ellipse'
+            steps[2].type === 'ellipse'
         );
     },
     code: '###\nAdd the \'move\' spell, it will change\nthe position of the circle\n###\n\ncolor "yellow"\ncircle 20',