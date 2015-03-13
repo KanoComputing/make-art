KanoComputing/make-art

commit b333a3feace048163bbd9403bd3669c8b7c761d2

Clean up, split offline and online modules, integrate basic login and progress loading

lib/controller/export.js
@@ -1,5 +1,5 @@
 var app = require('../app'),
-    challengeIO = require('../api/challenge-io');
+    api = require('../api');
 
 app.controller('export', function($scope, $window) {
     var win = angular.element($window),
@@ -64,11 +64,7 @@ app.controller('export', function($scope, $window) {
         $scope.isOpen = true;
     };
 
-    if ($scope.dialog === 'save') {
-        $scope.exportFn = challengeIO.local.save;
-    } else {
-        $scope.exportFn = challengeIO.web.share;
-    }
+    $scope.exportFn = api.challengeIO.save;
 
     $scope.export = function() {
         $scope.exportFn(
@@ -83,7 +79,7 @@ app.controller('export', function($scope, $window) {
     $scope.exportWallpaper = function() {
         var wallpapers = getWallpapers();
 
-        challengeIO.local.saveWallpaper(
+        api.challengeIO.local.saveWallpaper(
             $scope.filename,
             wallpapers['1024'],
             wallpapers['4-3'],

gulpfile.js
@@ -12,7 +12,8 @@ var gulp = require('gulp'),
 
 var server = lr(),
     env = process.env.NODE_ENV === 'production' ? 'production' : 'develpoment',
-    production = env === 'production';
+    production = env === 'production',
+    offline = env.OFFLINE === 'true';
 
 var paths = {
     views      : { watch: [ 'views/**/*.jade', 'content/**/*' ], src: 'views/**/*.jade', out: 'www' },
@@ -57,10 +58,11 @@ gulp.task('styles', function () {
 gulp.task('views', function () {
     gulp.src(paths.views.src)
     .pipe(jade({
-        pretty: !production,
-        locals: _.extend({
-            env: env,
-            production: production
+        pretty : !production,
+        locals : _.extend({
+            env        : env,
+            production : production,
+            offline    : offline
         }, jadeHelpers)
     }))
     .on('error', handleError)

lib/api.js
@@ -1,0 +1,5 @@
+var config = require('./core/config'),
+    offline = require('./api/offline/index'),
+    online = require('./api/online/index');
+
+module.exports = config.OFFLINE ? offline : online;

lib/api/challenge-io.js
@@ -1,47 +1,0 @@
-var api = require('./local-api'),
-    notify = require('./notify');
-
-var local = {
-    save: function(filename, code, desc, image) {
-        api.challenge.local.save({
-            filename: filename,
-            code: code,
-            description: desc,
-            image: image
-        })
-        .then(notify.success, notify.failure);
-    },
-    saveWallpaper: function(filename, image_1024, image_4_3, image_16_9) {
-        api.challenge.local.saveWallpaper({
-            filename: filename,
-            image_1024: image_1024,
-            image_4_3: image_4_3,
-            image_16_9: image_16_9
-        })
-        .then(notify.success, notify.failure);
-    }
-};
-
-var web = {
-    share: function(filename, code, desc, image) {
-        api.challenge.web.share({
-            filename: filename,
-            code: code,
-            description: desc,
-            image: image
-        })
-        .then(notify.success, notify.failure);
-    },
-    load: function(cb) {
-        api.challenge.web.load()
-        .then(function (res) {
-            cb(res.body);
-        }, notify.failure);
-    }
-};
-
-
-module.exports = {
-    local : local,
-    web   : web
-};

lib/api/offline/auth.js
@@ -1,0 +1,7 @@
+module.exports = {
+    init: function (callback) {
+        callback(null, {
+            username : 'foo'
+        });
+    }
+};

lib/api/offline/challenge-io.js
@@ -1,0 +1,38 @@
+var api = require('./local-api'),
+    notify = require('./notify');
+
+module.exports = {
+    save: function(filename, code, desc, image) {
+        api.challenge.save({
+            filename    : filename,
+            code        : code,
+            description : desc,
+            image       : image
+        })
+        .then(notify.success, notify.failure);
+    },
+    share: function(filename, code, desc, image) {
+        api.challenge.web.share({
+            filename    : filename,
+            code        : code,
+            description : desc,
+            image       : image
+        })
+        .then(notify.success, notify.failure);
+    },
+    load: function(cb) {
+        api.challenge.web.load()
+        .then(function (res) {
+            cb(res.body);
+        }, notify.failure);
+    },
+    saveWallpaper: function(filename, image_1024, image_4_3, image_16_9) {
+        api.challenge.saveWallpaper({
+            filename   : filename,
+            image_1024 : image_1024,
+            image_4_3  : image_4_3,
+            image_16_9 : image_16_9,
+        })
+        .then(notify.success, notify.failure);
+    }
+};

lib/api/offline/index.js
@@ -1,0 +1,6 @@
+module.exports = {
+    auth        : require('./auth'),
+    progress    : require('./progress'),
+    server      : require('./server'),
+    challengeIO : require('./challenge-io')
+};

commit 6296afee385b3afd5d2c952724e2136b30eb3564

Fix exporting wallpapers
Exporting wallpaper functionality was broken by renaming in commit
b333a3feace048163bbd9403bd3669c8b7c761d2. Fix calls to this function.

lib/controller/export.js
@@ -104,7 +104,7 @@ app.controller('ExportController', function($scope, $window, $rootScope) {
     $scope.exportWallpaper = function() {
         var wallpapers = getWallpapers();
 
-        api.challengeIO.local.saveWallpaper(
+        api.challengeIO.saveWallpaper(
             $scope.filename,
             wallpapers['1024'],
             wallpapers['4-3'],