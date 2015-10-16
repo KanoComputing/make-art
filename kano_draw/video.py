05b050a4af9f6d5628e42fa884359eaa4d8d0ebf
Costas Voloudakis <costas@kano.me>
2015-10-16 15:59:54 +0100

    Update progress checking fn for video
    This made use of the old way of checking for level progress.
    Update it to use the newly introduced groups

--- kano_draw/video.py
+++ kano_draw/video.py
@@ -13,15 +13,25 @@ VIDEO_PATH = '/usr/share/kano-media/videos/art.mp4'
 
 
 def play_intro():
-    level = load_app_state_variable(APP_NAME, 'level')
-    # Skip the intro in case the user progressed past level 1
-    if level > 0:
-        return
+    # Skip the intro in case the user progressed level 1
+    has_progressed = False
 
-    try:
-        from kano_video.logic.player import play_video
-        # stop the splash screen before the video starts
-        os.system('/usr/bin/kano-stop-splash')
-        play_video(localfile=VIDEO_PATH)
-    except ImportError:
-        pass
+    groups = load_app_state_variable(APP_NAME, 'groups')
+    if groups is not None and isinstance(groups, dict):
+        for group in groups.itervalues():
+            if 'challengeNo' in group and group['challengeNo'] > 0:
+                has_progressed = True
+                break
+    else:
+        level = load_app_state_variable(APP_NAME, 'level')
+        if level > 0:
+            has_progressed = True
+
+    if not has_progressed:
+        try:
+            from kano_video.logic.player import play_video
+            # stop the splash screen before the video starts
+            os.system('/usr/bin/kano-stop-splash')
+            play_video(localfile=VIDEO_PATH)
+        except ImportError:
+            pass
