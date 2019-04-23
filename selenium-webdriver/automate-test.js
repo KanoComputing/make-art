adb56817af531805fefe669d5c29fc26c336bf47
Laura Enria <enria.laura2@gmail.com>
2019-04-23 18:30:00 +0100

    feat: work in progress adding test in Selenium-WebDriver

--- selenium-webdriver/automate-test.js
+++ selenium-webdriver/automate-test.js
@@ -1,22 +1,39 @@
-let webdriver = require('selenium-webdriver'),
-  By = webdriver.By,
-  until = webdriver.until;
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+let myServer = 'http://172.16.254.110:3000'
 
 let chrome = require('selenium-webdriver/chrome')
 let chrome_options = new chrome.Options();
 
 chrome_options.setChromeBinaryPath('/usr/bin/chromium-browser');
 
-let driver_chr = new webdriver.Builder()
+let driver_builder = new Builder()
   .forBrowser('chrome')
   .setChromeOptions(chrome_options)
-  .build();
+
+let browserLaunchStartTime = process.hrtime()
+let browserLaunchDuration = 0; // it will retutn an array => [seconds, nanoseconds]
+// console.log('browserLaunchStartTime =>', browserLaunchStartTime);
+
+let driver_chr = driver_builder.build();
 
 const searchTest = (driver, website) => {
   driver.get(website);
 
-  driver.findElement(By.css('button.button.large')).click();
 
+  // test 1: Check the performance of the lunching browser 
+  driver.wait(until.elementLocated(By.css('button.button.large')))
+  browserLaunchDuration = process.hrtime(browserLaunchStartTime)
+  console.log('browserLaunchDuration =>', browserLaunchDuration[0], 's', browserLaunchDuration[1], 'ns')
+
+
+  //
+
+  driver.findElement(By.css('button.button.large')).click()
+  driver.findElement(By.css('a.world-cover')).click()
+  driver.wait(until.elementLocated(By.css('div.cover'))).click()
+
+  // test 2: 
   // driver.sleep(1000).then(function () {
   //   driver.findElement(By.name('q')).sendKeys(webdriver.Key.TAB);
   // });
@@ -31,11 +48,10 @@ const searchTest = (driver, website) => {
   //       console.log('Test failed');
   //     }
   //     driver.quit();
-    // });
+  // });
   // });
 }
 
-let myServer = 'http://172.16.254.110:3000'
 
 searchTest(driver_chr, myServer);
 
