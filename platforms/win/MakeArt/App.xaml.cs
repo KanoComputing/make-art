8a3199375e3bd9e9196911a0eed86e4e9d388678
Tom Bettany <tom@kano.me>
2019-06-06 11:50:18 +0100

    Win: Add UWP app
    Create wrapper UWP app for browsing Make Art on the Web.

---
+++ platforms/win/MakeArt/App.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Foundation.Metadata;
+using System.Diagnostics;
+using Windows.ApplicationModel.Background;
+using System.Threading.Tasks;
+
+using Windows.ApplicationModel.Resources;
+using System.Net.NetworkInformation;
+
+namespace MakeArt
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        private Logger Log = Logger.getLogger();
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+            var resources = new ResourceLoader("config");
+            var baseUri = resources.GetString("BASE_URL");
+
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            localSettings.Values["BASE_URI"] = baseUri;
+        }
+
+        void SetupApp()
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Log.Channel.LogEvent("App Launched");
+            SetupApp();
+
+            bool canEnablePrelaunch = ApiInformation.IsMethodPresent("Windows.ApplicationModel.Core.CoreApplication", "EnablePrelaunch");
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (e.PreviousExecutionState == ApplicationExecutionState.Terminated) {
+                //TODO: Load state from previously suspended application
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                // On Windows 10 version 1607 or later, this code signals that this app wants to participate in prelaunch
+                if (canEnablePrelaunch) {
+                    TryEnablePrelaunch();
+                }
+                if (rootFrame.Content == null) {
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
+                }
+            }
+            // If the API is present (doesn't exist on 10240 and 10586)
+            if (ApiInformation.IsPropertyPresent(typeof(LaunchActivatedEventArgs).FullName, nameof(LaunchActivatedEventArgs.TileActivatedInfo))) {
+                // If clicked on from tile
+                if (e.TileActivatedInfo != null) {
+                    // If tile notification(s) were present
+                    if (e.TileActivatedInfo.RecentlyShownNotifications.Count > 0) {
+                        // Get arguments from the notifications that were recently displayed
+                        string[] allArgs = e.TileActivatedInfo.RecentlyShownNotifications.Select(i => i.Arguments).ToArray();
+
+                        if (allArgs.Length > 0) {
+                            OpenProject(allArgs[0]);
+                        }
+                    }
+                }
+            }
+
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            var uri = localSettings.Values["BASE_URI"] as string;
+            this.OpenProject(uri);
+        }
+
+        private void TryEnablePrelaunch()
+        {
+            Windows.ApplicationModel.Core.CoreApplication.EnablePrelaunch(true);
+        }
+
+        protected override void OnActivated(IActivatedEventArgs e)
+        {
+            SetupApp();
+            if (e is ToastNotificationActivatedEventArgs)
+            {
+                Frame rootFrame = Window.Current.Content as Frame;
+                var toastActivationArgs = e as ToastNotificationActivatedEventArgs;
+                OpenProject(toastActivationArgs.Argument);
+            }
+        }
+
+        void OpenProject(string uri)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame != null)
+            {
+                bool isInternetConnected = NetworkInterface.GetIsNetworkAvailable();
+                if (!isInternetConnected) {
+                    rootFrame.Navigate(typeof(Offline), uri);
+                } else {
+                    rootFrame.Navigate(typeof(MainPage), uri);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            Log.Channel.LogEvent("Suspending App");
+            Log.Dispose();
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
