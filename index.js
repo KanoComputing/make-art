20982bca8132caacfb4a1262d6f0d24e70a1b9e1
Laura Enria <lauraenria@users.noreply.github.com>
2019-07-17 17:59:29 +0100

    [PC-160] Launching draw files by clicking them is unreliable (#443)
    On Windows, it is possible to launch the app by clicking on one of the share files that you have downloaded separately. This uses a mechanism which Windows exposes into the JS and was being triggered at the same time as when we initialise the app, causing a race condition. Create an Angular service for handling this activation so that it can be called when we are ready for it rather than when it finishes.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 class MakeArt {
     constructor(bus, config) {
         this.root = document.createElement('div');
@@ -11,57 +13,8 @@ class MakeArt {
             config.APP_ROOT = `${config.UI_ROOT}www/`;
             window.MakeArt.app.constant('_config', config);
             window.MakeArt.bootstrap(this.root);
-            if (config.launchActivatedEventArgs) {
-                this.handleActivation(config.launchActivatedEventArgs)
-            }
         });
-    }
-    handleActivation(args) {
-        const { ActivationKind } = Windows.ApplicationModel.Activation;
-        const { StandardDataFormats } = Windows.ApplicationModel.DataTransfer;
-        if (args.kind === ActivationKind.file) {
-            const [file] = args.files;
-            this.loadFile(file);
-        } else if (args.kind === ActivationKind.shareTarget) {
-            const { data } = args.shareOperation
-            if (data.contains(StandardDataFormats.storageItems)) {
-                data.getStorageItemsAsync()
-                    .done((items) => {
-                        const [item] = items;
-                        this.shareFile(item, data.properties);
-                    });
-            }
-        }
-    }
-    readFile(file) {
-        return file.openReadAsync()
-            .then((stream) => {
-                const inputStream = stream.getInputStreamAt(0);
-                var dataReader = new Windows.Storage.Streams.DataReader(inputStream);
-                return dataReader.loadAsync(stream.size)
-                    .then((loaded) => {
-                        const text = dataReader.readString(loaded);
-                        return text;
-                    });
-            });
-    }
-    loadFile(file) {
-        return this.readFile(file)
-            .then((text) => {
-                window.MakeArt.app.loadCode(text);
-            });
-    }
-    shareFile(file, properties) {
-        return this.readFile(file)
-            .then((text) => {
-                const share = {
-                    code: text,
-                    title: properties.title,
-                    description: properties.description,
-                }
-                window.MakeArt.app.shareCode(share);
-            });
-    }
+    }    
 }
 
 Shell.define(MakeArt);
--- utils/find-app.js
+++ utils/find-app.js
@@ -1,6 +1,5 @@
 const fg = require('fast-glob');
 const path = require('path');
-
 fg('./out/*.appxbundle', { cwd: path.join(__dirname, '../') })
     .then((results) => {
         console.log(results[0]);
