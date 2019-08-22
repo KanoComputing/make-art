14065a584e803f45b2e7adcf8d15f2883c949950
Paul Varache <paul@kano.me>
2019-08-22 17:28:31 +0100

    feat: add telemetry

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 "use strict";
+import { Telemetry } from './lib/core/telemetry.js';
+import { TelemetryReporter } from './lib/core/reporter.js';
 
 class MakeArt {
     constructor(kash) {
@@ -11,6 +13,8 @@ class MakeArt {
         link.rel = 'stylesheet';
         link.href = `${config.UI_ROOT}www/css/main.css`;
         document.head.appendChild(link);
+        this.telemetryReporter = new TelemetryReporter(config.TELEMETRY_URL);
+        this.telemetryReporter.start(Telemetry);
         import('./lib/index.js').then(() => {
             config.APP_ROOT = `${config.UI_ROOT}www/`;
             window.MakeArt.app.constant('_config', config);
