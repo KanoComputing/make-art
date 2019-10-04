ec992e3a6fec2b948aeb1965df7430d71f048d2e
Laura Enria <enria.laura2@gmail.com>
2019-10-04 12:00:37 +0100

    pull the branch name directly from the build

--- jenkins/kano-os.groovy
+++ jenkins/kano-os.groovy
@@ -6,7 +6,7 @@ node('os') {
      stage('build') {
          dr {
              withCredentials([string(credentialsId: 'npm-read-only', variable: 'NPM_TOKEN')]) {
-                 sh script: "set -e; dr build kano-draw -b OS-1064/jenkins-built-process-for-KanoOS"
+                 sh script: "dr build kano-draw -b ${env.BRANCH_NAME}"
                  
              }
          }
